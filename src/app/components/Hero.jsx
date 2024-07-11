7470d70ba2a8338ab1a8c4be62679b8702c3a66d
hamzahashmi033 <hashmihamza846@gmail.com>
2024-07-11 16:05:31 +0500

    changes added

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -11,18 +11,18 @@ const Hero = () => {
       <div className="sm:container lg:container  xs:px-4 sm:px-8 lg:px-4  relative  sm:mx-auto  ">
         <Nav />
         <Image
-          src={"/Isolation_Mode.png"}
-          width={800}
+          src={"/Isolation_Mode.svg"}
+          width={200}
           height={300}
-          className="absolute max-w-[370px] 2xl:max-w-[800px] xl:max-w-[500px] top-0 right-0"
+          className="absolute xs:w-[900px] sm:w-[600px] 2xl:w-[900px] top-0 xs:right-[-120px] sm:right-[-290px]"
         />
-        <div className="font-ClashDisplay 2xl:mt-20 sm:mt-12 xs:mt-4">
+        <div className="font-ClashDisplay xs:leading-[25px] sm:leading-[30px] xl:leading-[40px] 2xl:leading-[60px] 2xl:mt-32 sm:mt-0 md:mt-8  xs:mt-16">
           <h1 className="text-white  font-ClashDisplay ">
             <span className="2xl:text-[30px]  xl:text-[20px]  sm:text-[18px] font-[300]">
               Turning Your Challenges
             </span>
             <div className="flex ">
-              <span className="sm:tracking-wider 2xl:text-[67px] xl:text-[48px] xs:text-[24px] text-Red font-[400] font-Neuropal">
+              <span className="sm:tracking-wider 2xl:text-[67px] xl:text-[48px] xs:text-[24px] sm:text-[32px] text-Red font-[400] font-Neuropal">
                 <Typewriter
                   options={{
                     strings: ["Into Innovative"],
@@ -31,35 +31,35 @@ const Hero = () => {
                   }}
                 />
               </span>
-              <span className="text-White font-[400] font-Neuropal xs:text-[24px] xl:text-[48px] 2xl:text-[67px]">
+              <span className="text-White font-[400] font-Neuropal xs:text-[24px] sm:text-[32px] xl:text-[48px] 2xl:text-[67px]">
                 &nbsp;Solutions
               </span>
             </div>
 
-            <span className="text-White font-[500] 2xl:text-[67px] xl:text-[48px] xs:text-[24px]">
+            <span className="text-White font-ClashDisplay font-[500] sm:text-[32px] 2xl:text-[67px] xl:text-[48px] xs:text-[24px]">
               Samurai Systems
             </span>
             <Image
               src={"/lines.svg"}
               width={500}
               height={100}
-              className="2xl:w-[500px] xs:w-[200px] xl:w-[450px]"
+              className="2xl:w-[500px] xs:w-[200px] xl:w-[320px]"
             />
           </h1>
-          <p className="text-White xs:mt-8 2xl:mt-16 font-[300] xs:text-[14px] lg:text-[18px] 2xl:text-[24px]">
+          <p className="text-White xs:mt-8 2xl:mt-16 font-[300] xs:text-[14px] sm:text-[16px] lg:text-[18px] 2xl:text-[24px]">
             Samurai -Team of solution architects, developers and consultants
             having only one aim in mind
             <br />
-            <span className="text-White font-[500] xs:text-[14px] lg:text-[18px] 2xl:text-[24px]">
+            <span className="text-White font-[500] xs:text-[14px] sm:text-[16px] lg:text-[18px] 2xl:text-[24px]">
               “deliver the best, nothing else”
             </span>
           </p>
-        </div>
-        <div className="xs:mt-14 2xl:mt-16  sm:mt-12 font-ClashDisplay flex gap-4">
-          <button className="bg-Red text-white xs:px-[16px] xs:py-[2px] xl:px-[32px] xl:py-[6px] 2xl:py-[10px] 2xl:px-[41px]">
+        </div>{" "}
+        <div className=" xs:mt-6  2xl:mt-10 font-[600] xs:text-[14px] sm:text-[16px] 2xl:text-[24px] md:mt-10 sm:mt-16 xl:mt-10 2 font-ClashDisplay flex gap-4">
+          <button className="bg-Red   text-white rounded-[5px] xs:px-[16px] xs:py-[2px] xl:px-[32px] xl:py-[6px] 2xl:py-[10px] 2xl:px-[41px]">
             Get a Quote
           </button>
-          <button className="border border-Red xl:px-[32px] xs:px-[16px] xs:py-[2px] xl:py-[6px] 2xl:py-[10px] 2xl:px-[41px] text-white">
+          <button className="border border-Red xl:px-[32px] sm: rounded-[5px] xs:px-[16px] xs:py-[2px] xl:py-[6px] 2xl:py-[10px] 2xl:px-[41px] text-white">
             Our Services
           </button>
         </div>
@@ -70,7 +70,7 @@ const Hero = () => {
               "linear-gradient(271.18deg, rgba(58, 58, 58, 0) -4.85%, #3a3a3a 147.42%)",
             borderImageSlice: "1",
           }}
-          className="text-white xs:flex-row font-ClashDisplay xs:p-2  sm:w-[50vw] 2xl:gap-16   flex gap-8 xs:gap-2 xs:mt-14 2xl:mt-40 xl:mt-4  sm:mt-10"
+          className="text-white xs:flex-row font-ClashDisplay  xs:p-2 2xl:text-[26px]  sm:w-[50vw] xl:px-4 xl:w-[30vw] xl:gap-4 2xl:w-[35vw] 2xl:px-6 2xl:py-2 2xl:gap-16   flex gap-8 xs:gap-2 xs:mt-24 2xl:mt-32 xl:mt-12 md:mt-16  sm:mt-14"
         >
           <p className="font-[300]">
             <span className="text-Red font-[600]">2000+</span>
--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,33 +1,10 @@
 "use client";
 
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { FiMenu } from "react-icons/fi";
 
 const Nav = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [dropdownOpen, setDropdownOpen] = useState({
-    it: false,
-    cyber: false,
-    digital: false,
-  });
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const toggleDropdown = (key) => {
-    setDropdownOpen((prevState) => ({ ...prevState, [key]: !prevState[key] }));
-  };
-
-  const menuItems = [
-    { label: "IT Infrastructure", key: "it" },
-    { label: "Cybersecurity", key: "cyber" },
-    { label: "Digital Transformation", key: "digital" },
-  ];
-
   return (
     <nav className="2xl:max-w-[1728px] text-white relative z-10">
       <div className="flex items-center justify-between py-5">
@@ -38,100 +15,34 @@ const Nav = () => {
               width={100}
               height={100}
               alt="Logo"
-              className="xl:w-[10rem] lg:w-[7rem] sm:w-[8rem] xs:w-[8rem]"
+              className="xl:w-[10rem] lg:w-[8rem] sm:w-[9rem] xs:w-[10rem]"
             />
           </a>
         </div>
-        <div className="flex items-center lg:hidden">
+        <div className="flex items-center md:order-2">
           <button
             type="button"
-            className="text-white bg-[#C92127] p-2 rounded lg:hidden"
+            className="text-white bg-[#C92127] p-2 xs:p-1 rounded md:font-normal xs:font-[8px] md:mr-4 sx:mr-1"
           >
             Get A Quote
           </button>
-          <button className="text-white ml-4" onClick={toggleMenu}>
-            <FiMenu size={24} />
-          </button>
-        </div>
-        <ul className="hidden lg:flex lg:gap-2 xl:gap-3">
-          {menuItems.map((item) => (
-            <li
-              key={item.key}
-              className="relative text-base cursor-pointer flex items-center font-normal font-ClashDisplay"
-              onClick={() => toggleDropdown(item.key)}
-            >
-              {item.label} <RiArrowDropDownLine />
-              {dropdownOpen[item.key] && (
-                <ul className="absolute top-full left-0 bg-black p-3">
-                  <li className="p-2">Service 1</li>
-                  <li className="p-2">Service 2</li>
-                  <li className="p-2">Service 3</li>
-                </ul>
-              )}
-            </li>
-          ))}
-          <li className="relative text-base cursor-pointer flex items-center font-normal font-ClashDisplay">
-            <a href="/about-us">About Us</a>
-          </li>
-        </ul>
-        <ul className="hidden lg:flex items-center gap-5">
-          <div className="flex items-center">
-            <div className="border border-white w-[80px] h-px flex-grow ml-2 hidden sm:block md:hidden xl:block" />
-            <li className="cursor-pointer ml-3">
-              <FaFacebookF size={22} />
-            </li>
-            <li className="cursor-pointer text-red-500 mx-3">
-              <FaLinkedinIn size={22} />
-            </li>
-            <li className="cursor-pointer mr-3">
-              <FaTwitter size={22} />
-            </li>
-            <div className="border border-white w-[80px] h-px flex-grow ml-2 hidden sm:block md:hidden xl:block" />
-          </div>
-        </ul>
-        <button
-          type="button"
-          className="hidden lg:block text-white bg-[#C92127] p-1 lg:p-3 rounded"
-        >
-          Get A Quote
-        </button>
-      </div>
-      {menuOpen && (
-        <div className="lg:hidden p-5 bg-black">
-          <ul className="flex flex-col gap-5">
-            {menuItems.map((item) => (
-              <li
-                key={item.key}
-                className="cursor-pointer flex flex-col items-start font-normal font-ClashDisplay"
-                onClick={() => toggleDropdown(item.key)}
-              >
-                <div className="flex items-center font-normal font-ClashDisplay">
-                  {item.label} <RiArrowDropDownLine />
-                </div>
-                {dropdownOpen[item.key] && (
-                  <ul className="mt-2 w-full bg-black p-3">
-                    <li className="p-2">Service 1</li>
-                    <li className="p-2">Service 2</li>
-                    <li className="p-2">Service 3</li>
-                  </ul>
-                )}
+          <ul className="flex items-center sm:gap-5 xs:hidden md:flex">
+            <div className="flex items-center">
+              <div className="border border-white w-[80px] h-px flex-grow ml-2 hidden sm:block md:hidden xl:block" />
+              <li className="cursor-pointer ml-3">
+                <FaFacebookF size={22} />
               </li>
-            ))}
-            <li className="cursor-pointer">
-              <a href="/about-us">About Us</a>
-            </li>
-            <li className="cursor-pointer">
-              <FaFacebookF />
-            </li>
-            <li className="cursor-pointer text-red-500">
-              <FaLinkedinIn />
-            </li>
-            <li className="cursor-pointer">
-              <FaTwitter />
-            </li>
+              <li className="cursor-pointer text-red-500 md:mx-3 xs:mx-1">
+                <FaLinkedinIn size={22} />
+              </li>
+              <li className="cursor-pointer mr-3">
+                <FaTwitter size={22} />
+              </li>
+              <div className="border border-white w-[80px] h-px flex-grow ml-2 hidden sm:block md:hidden xl:block" />
+            </div>
           </ul>
         </div>
-      )}
+      </div>
     </nav>
   );
 };
